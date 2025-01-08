3c1efb30bbb69754152d297e6db9b1272a761426
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2025-01-08 22:36:15 +0200

    feate: add responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT:'15px',
+      }
+    },
+    screens:{
+      sm:'640px',
+      md:'768px',
+      lg:'960px',
+      xl:'1200px',
+    },
     extend: {
+      colors:{
+        primary:'#242a2b',
+        secondary:'#808080',
+        accent:{
+          DEFAULT:'#1cbccf',
+          secondary:'#18abbd',
+          tertiary: '#90c6cd',
+        },
+        gray:'#e8f0f1',
+      },
+      fontFamily:{
+        primary:'Poppins',
+      },
+      boxShadow:{
+        custom1:'0px 2px 40px 0px rgba(8,70,78,0.08)',
+        custom2:'0px 0px 30px 0px rgba(8,73,78,0.06)',
+      },
+      backgroundImage:{
+        service:'url(../../assets/img/services.svg)',
+        testimonials:'url(../../assets/img/testimonials.svg)',
+        departments:'url(../../assets/img/departments.svg)',
+        qouteLeft:'url(../../assets/img/testimonials/qoute-left.svg)',
+        qouteRight:'url(../../assets/img/testimonials/qoute-right.svg)',
+      }
     },
   },
   plugins: [],
