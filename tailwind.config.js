d3fa38709ee354744f2baa9413db8c4724e74d7a
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2025-01-08 12:01:59 +0200

    config: add main files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
